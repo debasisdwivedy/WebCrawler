2582d9bca007234cfb9bbedfec371b78db1e3a90
Debasis Dwivedy <ddwivedy@iu.edu>
2017-06-12 12:47:59 -0400

    Html Parser

---
+++ Parser.js
@@ -0,0 +1,74 @@
+
+var method=Parser.prototype;
+var htmlparser = require("htmlparser2");
+var http= require('http');
+var https = require('https');
+var Set = require('./Set.js');
+
+var data='<!DOCTYPE html><html><head></head><body>Hi there!</body></html>';
+
+function Parser(baseUrl) {
+    _self=this;
+    _self._baseUrl=baseUrl;
+    _self._protocol=http;
+    _self.s = new Set();
+}
+
+method.getProtocol = function(url)
+{
+    if(url.indexOf("https")>=0)
+    {
+        this._protocol=https;
+    }
+
+}
+
+method.isCompleteUrl = function(url)
+{
+    if(url.indexOf("/")==0)
+    {
+        return this._baseUrl+url;
+    }
+    return url;
+}
+
+method.parsedData = new htmlparser.Parser({
+    onopentag: function (name,attribute) {
+        if(name == 'a')
+        {
+            var links=_self.isCompleteUrl(attribute.href);
+            _self.s.addObj(links);
+        }
+
+    },
+    onend: function () {
+        console.log(_self.s.details());
+    },
+},{decodeEntities : true});
+
+
+method.getParsedData= function(data)
+{
+    this.parsedData.write(data);
+    this.parsedData.end();
+}
+
+method.getResponse= function(url)
+{
+    var data='';
+    this.getProtocol(url);
+    this._protocol.get(url,(function (response) {
+
+        response.on('data', (function (chunk) {
+            data += chunk;
+        }).bind(this))
+
+        response.on('end', (function () {
+            //console.log("Data is .."+ data);
+            this.getParsedData(data);
+        }).bind(this))
+
+    }).bind(this))
+}
+
+module.exports = Parser;
